c0da02753aabb7f79645b4678f74bbec365ead70
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2023-02-28 19:29:41 +0000

    Fixed some memory leaks

--- srcs/main/free.c
+++ srcs/main/free.c
@@ -1,9 +1,35 @@
 #include"../../inc/minirt.h"
 
+void free_matrix(float **array, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+void	free_view_matrix(t_camera *cameras, int nb_cameras)
+{
+	int	i;
+
+	i = 0;
+	while (i < nb_cameras)
+	{
+		free_matrix(cameras[i].view_matrix, 4);
+		i++;
+	}
+}
+
 int	free_all(t_data *data)
 {
 	mlx_destroy_window(data->mlx, data->mlx_win);
 	free(data->nb_objs);
+	free_view_matrix(data->scene->cameras, data->nb_objs->nb_cameras);
 	free(data->scene->spheres);
 	free(data->scene->planes);
 	free(data->scene->cylinders);
